69657f10618857e219e8e31bf972d3233626413f
Norrie Adams <normanadams087@gmail.com>
2026-04-30 12:56:18 +0000

    Added User Input that displays on the board

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
-
 #include <iostream>
 using namespace std;
 
-void drawBoard() {
-    char board[10][10]; 
-    
+char board[10][10]; 
+
+void initializeBoard() {
     for (int row = 0; row < 10; row++) {
         for (int col = 0; col < 10; col++) {
             board[row][col] = '-';
         }
     }
+}
 
+void drawBoard() {
     cout << "  "; 
     for (int col = 0; col < 10; col++) {
         cout << col << " ";
@@ -19,7 +20,6 @@ void drawBoard() {
 
     for (int row = 0; row < 10; row++) {
         cout << row << " "; 
-
         for (int col = 0; col < 10; col++) {
             cout << board[row][col] << " ";
         }
@@ -27,7 +27,26 @@ void drawBoard() {
     }
 }
 
+void updateBoard(int r, int c) {
+    if (r >= 0 && r < 10 && c >= 0 && c < 10) {
+        board[r][c] = 'X'; 
+    } else {
+        cout << "Out of bounds!" << endl;
+    }
+}
+
 int main() {
+    initializeBoard(); 
     drawBoard();
+
+    int rowInput;
+    int colInput;
+
+    cout << "Enter a coordinate pair (Row and Col separated by space. Ex: 4 5): " << endl;
+    cin >> rowInput >> colInput;
+
+    updateBoard(rowInput, colInput);
+    drawBoard(); 
+
     return 0;
 }
